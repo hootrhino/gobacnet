4f015aa379eeaa71658596c21ca2b9ae18aa4bf4
liyexin <liyexin@gemdale.com>
2024-05-31 11:47:43 +0800

    feat: support decoder ReadMultiProperty

--- encoding/readmultiple.go
+++ encoding/readmultiple.go
@@ -1,6 +1,7 @@
 package encoding
 
 import (
+	"fmt"
 	"github.com/hootrhino/gobacnet/btypes"
 )
 
@@ -20,4 +21,66 @@ func (e *Encoder) ReadMultipleProperty(invokeID uint8, data btypes.MultiplePrope
 	}
 
 	return e.Error()
+}
+
+func (d *Decoder) ReadMultipleProperty(data *btypes.MultiplePropertyData) error {
+	// Must have at least 7 bytes
+	if d.buff.Len() < 7 {
+		return fmt.Errorf("missing parameters")
+	}
+
+	data.Objects = make([]btypes.Object, 0)
+	objectIdx := 0
+	newObject := false
+	var tag uint8
+	var meta tagMeta
+	for {
+		if d.buff.Len() == 0 {
+			break
+		}
+		if newObject == false {
+			// first object identifier
+			tag, meta = d.tagNumber() // tag number = 0
+		}
+		newObject = false
+		fmt.Printf("isSpecifitag=%v tagNumber=%v len=%v\n", meta.isContextSpecific(), tag, d.value(meta))
+		objectType, instance := d.objectId()
+		fmt.Println(objectType, instance)
+		data.Objects = append(data.Objects, btypes.Object{
+			ID: btypes.ObjectID{
+				Type:     objectType,
+				Instance: instance,
+			},
+			Properties: []btypes.Property{},
+		})
+
+		// list of property references
+		for {
+			if d.buff.Len() == 0 {
+				break
+			}
+			tag, meta := d.tagNumber() // tag number = 1
+			if tag == 0 {
+				// 另一个object
+				newObject = true
+				break
+			}
+			fmt.Printf("isSpecifitag=%v tagNumber=%v len=%v\n", meta.isContextSpecific(), tag, d.value(meta))
+
+			tag, meta = d.tagNumber() // tag number = 0
+			fmt.Printf("isSpecifitag=%v tagNumber=%v len=%v\n", meta.isContextSpecific(), tag, d.value(meta))
+
+			var prop btypes.Property
+			prop.Type = btypes.PropertyType(d.enumerated(int(d.value(meta))))
+			fmt.Printf("property=%v\n", prop)
+			data.Objects[objectIdx].Properties = append(data.Objects[objectIdx].Properties, prop)
+
+			tag, meta = d.tagNumber() // tag number = 1
+			fmt.Printf("isSpecifitag=%v tagNumber=%v len=%v\n", meta.isContextSpecific(), tag, d.value(meta))
+		}
+
+		objectIdx++
+	}
+
+	return nil
 }
