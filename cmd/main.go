4ca2ebe8dc3219bb3a3eef9d51ef015276132e76
wwhai <cnwwhai@gmail.com>
2024-06-02 17:17:23 +0800

    dev: update

--- cmd/main.go
+++ cmd/main.go
@@ -25,16 +25,27 @@ func main() {
 		VendorId:   0x00_01_02_03,
 		NetWorkId:  1000, // 1-65,534
 		PropertyData: map[uint32][2]btypes.Object{
-			1:  apdus.NewAIPropertyWithRequiredFields("temp", 1, float32(3.14), "-/-"),
-			2:  apdus.NewAIPropertyWithRequiredFields("humi", 2, float32(77.67), "-/-"),
-			3:  apdus.NewAIPropertyWithRequiredFields("pres", 3, float32(101.11), "-/-"),
-			4:  apdus.NewAIPropertyWithRequiredFields("altitude", 4, float32(0), "-/-"),
-			5:  apdus.NewAIPropertyWithRequiredFields("altitude", 5, float32(0), "-/-"),
-			6:  apdus.NewAIPropertyWithRequiredFields("altitude", 6, float32(0), "-/-"),
-			7:  apdus.NewAIPropertyWithRequiredFields("altitude", 7, float32(0), "-/-"),
-			8:  apdus.NewAIPropertyWithRequiredFields("altitude", 8, float32(0), "-/-"),
-			9:  apdus.NewAIPropertyWithRequiredFields("altitude", 9, float32(0), "-/-"),
-			10: apdus.NewAIPropertyWithRequiredFields("altitude", 10, float32(0), "-/-"),
+			1:  apdus.NewAIPropertyWithRequiredFields("property:1", 1, float32(3.14), "-/-"),
+			2:  apdus.NewAIPropertyWithRequiredFields("property:2", 2, float32(77.67), "-/-"),
+			3:  apdus.NewAIPropertyWithRequiredFields("property:3", 3, float32(101.11), "-/-"),
+			4:  apdus.NewAIPropertyWithRequiredFields("property:4", 4, float32(0), "-/-"),
+			5:  apdus.NewAIPropertyWithRequiredFields("property:5", 5, float32(0), "-/-"),
+			6:  apdus.NewAIPropertyWithRequiredFields("property:6", 6, float32(0), "-/-"),
+			7:  apdus.NewAIPropertyWithRequiredFields("property:7", 7, float32(0), "-/-"),
+			8:  apdus.NewAIPropertyWithRequiredFields("property:8", 8, float32(0), "-/-"),
+			9:  apdus.NewAIPropertyWithRequiredFields("property:9", 9, float32(0), "-/-"),
+			10: apdus.NewAIPropertyWithRequiredFields("property:10", 10, float32(0), "-/-"),
+			11: apdus.NewAIPropertyWithRequiredFields("property:11", 11, float32(0), "-/-"),
+			12: apdus.NewAIPropertyWithRequiredFields("property:12", 12, float32(0), "-/-"),
+			13: apdus.NewAIPropertyWithRequiredFields("property:13", 13, float32(0), "-/-"),
+			14: apdus.NewAIPropertyWithRequiredFields("property:14", 14, float32(0), "-/-"),
+			15: apdus.NewAIPropertyWithRequiredFields("property:15", 15, float32(0), "-/-"),
+			16: apdus.NewAIPropertyWithRequiredFields("property:16", 16, float32(0), "-/-"),
+			17: apdus.NewAIPropertyWithRequiredFields("property:17", 17, float32(0), "-/-"),
+			18: apdus.NewAIPropertyWithRequiredFields("property:18", 18, float32(0), "-/-"),
+			19: apdus.NewAIPropertyWithRequiredFields("property:19", 19, float32(0), "-/-"),
+			20: apdus.NewAIPropertyWithRequiredFields("property:20", 20, float32(0), "-/-"),
+			21: apdus.NewAIPropertyWithRequiredFields("property:21", 21, float32(0), "-/-"),
 		},
 	})
 	client.SetLogger(logger)
@@ -44,8 +55,8 @@ func main() {
 	logger.Debug("client run success")
 	go func() {
 		for {
-			for i := 1; i <= 10; i++ {
-				newValue := rand.Float32()
+			for i := 1; i <= 20; i++ {
+				newValue := rand.Float32() * 10
 				client.GetBacnetIPServer().UpdateAIPropertyValue(uint32(i), newValue)
 				logger.Debug("Update Value: ", i, ", ", newValue)
 			}
